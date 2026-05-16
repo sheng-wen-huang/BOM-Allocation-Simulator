1f17731c4919f5992c2d0b51b0d6df50e7647f54
Huang Sheng Wen <shuang81@my.centennialcollege.ca>
2026-05-16 21:13:41 +0800

    Deploy Vite app to GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: '/BOM-Allocation-Simulator/',
   test: {
     environment: 'node',
     globals: true,
